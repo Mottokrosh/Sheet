a91ec37a437ff62b680b616041b2113f48ef832f
Frank Reding <frank.reding@gmail.com>
2013-12-06 17:00:08 +0000

    Successful resource read from db

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,20 +7,6 @@ angular.module('sheetApp')
 			fullName: 'Frank Reding'
 		};
 
-		$scope.character = Character.query({ '_id': {
-			'$oid': '52a20005e4b0d6f148055e45'
-		}});
-
-		/*angular.extend($scope.character, {
-			name: 'Grey Drosil',
-			alignment: 'Neutral Evil',
-			level: 'Summoner (Shadow Caller/Synthesist) 7'
-		});*/
-
-		$scope.$watch('character', function (a, b) {
-			console.log(a, b);
-		});
-
-		//$scope.character.$save();
+		$scope.character = Character.getById('52a20005e4b0d6f148055e45');
 
 	});
