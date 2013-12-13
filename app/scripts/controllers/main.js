5b629b9c77e1dcc5199580e4a48e93b41a2f8667
Frank Reding <frank.reding@gmail.com>
2013-12-13 09:36:15 +0000

    Load and display saved characters

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('sheetApp')
-	.controller('MainCtrl', function ($scope, user) {
+	.controller('MainCtrl', function ($scope, user, Character) {
 		$scope.user = user;
+
+		// get user's characters
+		$scope.characters = Character.query({ user: { id: user.id } }, function() {
+			angular.forEach($scope.characters, function (character) {
+				character.id = character._id.$oid;
+				if (!character.level) {
+					character.level = 'Unspecified class/level';
+				}
+			});
+		});
 	});
