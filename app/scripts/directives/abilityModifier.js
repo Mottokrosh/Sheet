9495ccf6d7a293118b54d8f821fc109e5ff219ce
Frank Reding <frank.reding@gmail.com>
2013-12-23 10:31:27 +0000

    Ability modifier directive, more form modelling

---
+++ app/scripts/directives/abilityModifier.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular.module('sheetApp')
+	.directive('abilityModifier', function () {
+		return {
+			templateUrl: 'views/directives/abilityModifier.html',
+			scope: {
+				ability: '@',
+				score: '=',
+				tempScore: '=',
+				show: '@',
+				label: '@'
+			},
+			restrict: 'E',
+			link: function link(scope, element, attrs) {
+				var watchAbility,
+					combined = false;
+
+				if (attrs.show === 'score') {
+					watchAbility = 'score';
+					scope.temp = false;
+				} else if (attrs.show === 'temp') {
+					watchAbility = 'tempScore';
+					scope.temp = true;
+				} else {
+					watchAbility = '[score,tempScore]';
+					combined = true;
+				}
+
+				scope.$watch(watchAbility, function(value) {
+					if (combined) {
+						// temp score takes precedence if present
+						value = value[1] ? value[1] : value[0];
+					}
+					var mod = Math.floor((parseInt(value, 10) - 10) / 2);
+					if (mod > 0) {
+						mod = '+' + mod;
+					}
+					scope.modifier = mod || '';
+				}, combined);
+			}
+		};
+	});
---
+++ test/spec/directives/abilityModifier.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Directive: abilityModifier', function () {
+
+	// load the directive's module
+	beforeEach(module('sheetApp'));
+
+	var element,
+		scope;
+
+	beforeEach(inject(function ($rootScope) {
+		scope = $rootScope.$new();
+	}));
+
+	/*it('should make hidden element visible', inject(function ($compile) {
+		element = angular.element('<ability-modifier></ability-modifier>');
+		element = $compile(element)(scope);
+		expect(element.text()).toBe('this is the abilityModifier directive');
+	}));*/
+});
